dd4fa6707288a742c6f14655c7165984b7a8263f
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-27 05:38:51 -0700

    shell

--- test_shell/ARGUMENTS/2-command_line_to_av.c
+++ test_shell/ARGUMENTS/2-command_line_to_av.c
@@ -5,24 +5,24 @@
  * main - splits a string and returns an array of each word of the string
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-    char *str1, *token;
-    int j;
+	char *buffer = NULL;
+	size_t len = 0;
+	char *token;
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s string delim\n",
-                argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	while (1)
+	{
+		printf("$ ");
+		getline(&buffer, &len, stdin);
+		printf("buffer getline: %s", buffer);
 
-    for (j = 1, str1 = argv[1]; ; j++, str1 = NULL) {
-        token = strtok(str1, argv[2]);
-        if (token == NULL)
-            break;
-        printf("%d: %s\n", j, token);
-    }
-
-    exit(EXIT_SUCCESS);
+		token = strtok(buffer, " ");
+		while (token)
+		{
+			printf("buffer strtok: %s\n", token);
+			token = strtok(NULL, " ");
+		}
+	}
+	return (0);
 }
-
